bb7e6a42d8a9552ac1f23371601b7cc93c93fab0
rahatnafees <rahatnafees4@gmail.com>
2024-11-10 04:05:24 +0500

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className='bg-[#A29875] p-4 w-[1920px]h-[134px]'>
+        <h1 className='text-[#FFFFFF] text-3xl pl-9 pt-5
+        w-[430px] h-[94px] font-normal text-[75px]  text-left'>
+        MANZZARI</h1>
+        
+
+        
+      
+    </div>
+  )
+}
+
+export default Header
